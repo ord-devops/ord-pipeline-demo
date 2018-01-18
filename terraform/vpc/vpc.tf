4c3cf995eccbcacca1836e344feccb87aef45bdc
Niels Albers <niels.albers@ordina.nl>
2018-01-18 14:08:12 +0100

    Fix security groups and refactor to separate rule format

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -15,19 +15,30 @@ resource "aws_security_group" "jumphost" {
   description = "jumphost allow ssh traffic"
   vpc_id      = "${module.vpc.vpc_id}"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "6"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags {
+    Name = "jumphost"
   }
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "jumphost_ssh" {
+  type = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  security_group_id = "${aws_security_group.jumphost.id}"
+  description = "allow ssh access to jumphost"
+}
+
+resource "aws_security_group_rule" "egress_jumphost" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "all"
+  cidr_blocks = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  security_group_id = "${aws_security_group.jumphost.id}"
 }
 
 resource "aws_security_group" "jenkins" {
@@ -35,58 +46,119 @@ resource "aws_security_group" "jenkins" {
   description = "jenkins allow ssh and jenkins traffic"
   vpc_id      = "${module.vpc.vpc_id}"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "6"
-    security_groups = ["${aws_security_group.jumphost.id}"]
-    self = true
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "6"
-    security_groups = ["${aws_security_group.alb.id}"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags {
+    Name = "jenkins"
   }
 }
 
+resource "aws_security_group_rule" "jenkins_ssh_jumphost" {
+  type = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  security_group_id = "${aws_security_group.jenkins.id}"
+  source_security_group_id = "${aws_security_group.jumphost.id}"
+  description = "allow ssh traffic from jumphost"
+}
+
+resource "aws_security_group_rule" "jenkins_ssh_self" {
+  type = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  security_group_id = "${aws_security_group.jenkins.id}"
+  self = true
+  description = "allow ssh traffic from self"
+}
+
+resource "aws_security_group_rule" "jenkins_http" {
+  type = "ingress"
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+  security_group_id = "${aws_security_group.jenkins.id}"
+  source_security_group_id = "${aws_security_group.alb.id}"
+  description = "allow http traffic on 8080 from alb"
+}
+
+resource "aws_security_group_rule" "jenkins_slave_agent_port" {
+  type = "ingress"
+  from_port  = 50000
+  to_port    = 50000
+  protocol   = "tcp"
+  security_group_id = "${aws_security_group.jenkins.id}"
+  source_security_group_id = "${aws_security_group.k8s.id}"
+  description = "allow slave agent traffic from kubernetes"
+}
+
+resource "aws_security_group_rule" "egress_jenkins" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "all"
+  cidr_blocks = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  security_group_id = "${aws_security_group.jenkins.id}"
+}
 
 resource "aws_security_group" "k8s" {
   name        = "kub8"
   description = "allow internal kubernetes traffic"
   vpc_id      = "${module.vpc.vpc_id}"
 
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "6"
-    security_groups = ["${aws_security_group.jumphost.id}","${aws_security_group.jenkins.id}"]
+  tags {
+    Name = "k8s"
   }
+}
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    security_groups = ["${aws_security_group.jenkins.id}"]
-    self = true
-  }
+resource "aws_security_group_rule" "k8s_ssh_jumphost" {
+  type = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  security_group_id = "${aws_security_group.k8s.id}"
+  source_security_group_id = "${aws_security_group.jumphost.id}"
+  description = "Allow incoming ssh from jumphost"
+}
 
+resource "aws_security_group_rule" "k8s_ssh_jenkins" {
+  type = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  security_group_id = "${aws_security_group.k8s.id}"
+  source_security_group_id = "${aws_security_group.jenkins.id}"
+  description = "Allow incoming ssh from jenkins"
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "k8s_jenkins" {
+  type = "ingress"
+  from_port   = 6443
+  to_port     = 6443
+  protocol    = "tcp"
+  security_group_id = "${aws_security_group.k8s.id}"
+  source_security_group_id = "${aws_security_group.jenkins.id}"
+  description = "allow jenkins traffic to kubernetes api on 6443"
+}
+
+resource "aws_security_group_rule" "k8s_self" {
+  type = "ingress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "all"
+  security_group_id = "${aws_security_group.k8s.id}"
+  self = true
+  description = "allow all traffic from kubernetes"
+}
+
+resource "aws_security_group_rule" "egress_k8s" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "all"
+  cidr_blocks = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  security_group_id = "${aws_security_group.k8s.id}"  
 }
 
 resource "aws_security_group" "alb" {
@@ -94,27 +166,44 @@ resource "aws_security_group" "alb" {
   description = "load balancer allow http and https traffic"
   vpc_id      = "${module.vpc.vpc_id}"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "6"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "6"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags {
+    Name = "alb"
   }
 }
+
+resource "aws_security_group_rule" "alb_http" {
+  type = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  security_group_id = "${aws_security_group.alb.id}"
+  description = "allow http traffic"
+}
+
+resource "aws_security_group_rule" "alb_https" {
+  type = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  security_group_id = "${aws_security_group.alb.id}"
+  description = "allow https traffic"
+}
+
+resource "aws_security_group_rule" "egress_alb" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "all"
+  cidr_blocks = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+  security_group_id = "${aws_security_group.alb.id}"  
+}
+
+
 
 output "vpc_id" {
   value = "${module.vpc.vpc_id}"
